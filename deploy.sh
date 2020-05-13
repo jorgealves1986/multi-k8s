d2aa198cab182a7e486d3be2bd5304c10a4581e5
jorge <jorge0.0alves@gmail.com>
2020-05-13 18:45:15 +0100

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+docker build -t jorge00alves/multi-client:latest -f ./client/Dockerfile ./client
+docker build -t jorge00alves/multi-server:latest -f ./server/Dockerfile ./server
+docker build -t jorge00alves/multi-worker:latest -f ./worker/Dockerfile ./worker
+
+docker push jorge00alves/multi-client:latest
+docker push jorge00alves/multi-server:latest
+docker push jorge00alves/multi-worker:latest
+
+kubectl apply -f k8s
+kubectl rollout restart deployment/client-deployment
+kubectl rollout restart deployment/server-deployment
+kubectl rollout restart deployment/worker-deployment
